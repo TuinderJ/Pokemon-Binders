c2dd879be8c3c0240b2111d42c7777c5c8d6de13
tuinderj <joshua.tuinder@gmail.com>
2025-12-11 19:06:51 -0700

    doc updates

--- spacetimedb/src/domain/mod.rs
+++ spacetimedb/src/domain/mod.rs
@@ -1,18 +1,30 @@
+/// This denotes a binder. The owner of the binder can add a list of desired cards, desired Pokemon, cards they've acquired, or Pokemon they've acquired.
+#[derive()]
 struct Binder {
+    /// Unique identifier of the binder.
     id: String,
+    /// The owner of the binder.
     owner: Owner,
+    /// The display name of the binder.
     name: String,
+    /// A list of Pokemon that the owner wants to collect in this binder.
     desired_pokemons: Vec<Pokemon>,
+    /// A list of Pokemon that the owner has collected in this binder.
     acquired_pokemons: Vec<AcquiredPokemon>,
+    /// A list of white-lists and/or black-lists for denoting what the owner is looking for.
+    /// There can be 1 list for each Pokemon that the owner has on their `desired` list.
     pokemon_restrictions: Vec<PokemonRestriction>,
+    /// A list of cards that the owner wants to collect in this binder.
     desired_cards: Vec<Card>,
+    /// A list of cards that the owner has collected in this binder.
     acquired_cards: Vec<AcquiredCard>,
 }
 
+/// The user information for the owner of one or more binders.
 struct Owner {
-    /// UUID of the binder owner
+    /// Unique identifier of the binder owner.
     id: String,
-    /// Display name of the binder owner
+    /// Display name of the binder owner.
     name: String,
     /// A list of binders that are designated as `Trade Binders`.
     trade_binders: Vec<Binder>,
@@ -20,26 +32,29 @@ struct Owner {
     binders: Vec<Binder>,
 }
 
+/// A helper struct to be able to select a card or cards that have been acquired when the owner is searching for Pokemon.
 struct AcquiredPokemon {
-    /// Generic Pokemon selection
+    /// Generic Pokemon selection.
     pokemon: Pokemon,
-    /// Specific cards that are in the binder
+    /// Specific cards that are in the binder.
     cards_acquired: Vec<AcquiredCard>,
 }
 
+/// Information about a specific Pokemon.
 struct Pokemon {
-    /// National Pokedex Number of the Pokemon
+    /// National Pokedex Number of the Pokemon.
     id: i16,
     /// Name of the Pokemon
     name: String,
 }
 
+/// A white-list or black-list for denoting which cards the owner deems acceptable for a specific Pokemon.
 struct PokemonRestriction {
-    /// Generic Pokemon selection
+    /// This points to the specific Pokemon that this list is for.
     pokemon: Pokemon,
     /// Whitelist or blacklist to designate if the owner wants to include or exclude specific cards.
     list_type: ListType,
-    /// Cards that the binder owner does not want to count toward a desired Pokemon
+    /// Cards that the binder owner does or does not want to count toward a desired Pokemon.
     list: Vec<Card>,
 }
 
@@ -48,14 +63,16 @@ enum ListType {
     BlackList,
 }
 
+/// This is to denote when a binder owner has acquired a specific card.
+/// More than one of the same card can be collected in the binder.
 struct AcquiredCard {
-    /// A specific Pokemon Card
+    /// A specific Pokemon Card.
     card: Card,
-    /// Quantity of this card in the binder
+    /// Quantity of this card in the binder.
     qty_acquired: i32,
 }
 
-/// A specific Pokemon Card
+/// Information about a specific Pokemon Card.
 struct Card {
     /// Unique identifier for the card.
     id: String,
@@ -79,28 +96,42 @@ struct Card {
     tcg_player: TCGPlayer,
 }
 
+/// A set of cards.
 struct Set {
+    /// A unique identifier for the set.
     id: String,
+    /// The name of the set.
     name: String,
-    printed_total: i16, // The number printed on the card that represents the total. This total does not include secret rares.
-    total: i16, // The total number of cards in the set, including secret rares, alternate art, etc.
-    release_date: String, // YYYY/MM/DD
+    /// The number printed on the card that represents the total. This total does not include secret rares.
+    printed_total: i16,
+    /// The total number of cards in the set, including secret rares, alternate art, etc.
+    total: i16,
+    /// The date the set was released (in the USA). Format is YYYY/MM/DD.
+    release_date: String,
+    /// Any images associated with the set, such as symbol and logo.
     images: SetImages,
 }
 
 struct SetImages {
-    symbol: String, // URL
-    logo: String,   // URL
+    /// The url to the symbol image.
+    symbol: String,
+    /// The url to the logo image.
+    logo: String,
 }
 
 struct CardImages {
-    small: String, // URL
-    large: String, // URL
+    /// The url to the small card image.
+    small: String,
+    /// The url to the large card image.
+    large: String,
 }
 
 struct TCGPlayer {
-    url: String,        // URL to purchase this card
-    updated_at: String, // A date that the price was last updated YYYY/MM/DD
+    /// The url to purchase this card.
+    url: String,
+    /// A date that the price was last updated YYYY/MM/DD.
+    updated_at: String,
+    /// The prices of this card as of the `updated_at` date.
     prices: CardPrices,
 }
 
