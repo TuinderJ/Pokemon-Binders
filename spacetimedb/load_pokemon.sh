37d29d8498d8d95c11e369d38f60f8fbd9e5c5d9
from11 <joshua.tuinder@gmail.com>
2025-12-18 13:03:49 -0700

    added a utility to load all pokemon into the pokemon table

--- spacetimedb/load_pokemon.sh
+++ spacetimedb/load_pokemon.sh
@@ -1,10 +1,11 @@
-curl "https://pokeapi.co/api/v2/pokemon?limit=2" | jq -c '.results[]' | while read -r pokemons; do
+curl -s "https://pokeapi.co/api/v2/pokemon?limit=1025" | jq -c '.results[]' | while read -r pokemons; do
   url=$(echo "$pokemons" | jq -r '.url')
-  curl "$url" | jq -c '{id: .id, name: .name, sprite: .sprites.front_default}' | while read -r pokemon; do
+  curl -s "$url" | jq -c '{id: .id, name: .name, sprite: .sprites.front_default}' | while read -r pokemon; do
     name=$(echo "$pokemon" | jq -r '.name')
     id=$(echo "$pokemon" | jq -r '.id')
     sprite=$(echo "$pokemon" | jq -r '.sprite')
-    echo "$name, $id, $sprite"
+    echo "Adding $id: $name..."
+    spacetime call -s local pokemon-binders add_pokemon "$id" "$name" "$sprite"
   done
 done
 
