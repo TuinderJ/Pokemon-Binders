bedaa30cb9eefadf87e18750bcd4517c69a217f6
from11 <joshua.tuinder@gmail.com>
2025-12-17 20:02:18 -0700

    made another iteration to test pokemon parsing from pokeapi

--- spacetimedb/load_pokemon.sh
+++ spacetimedb/load_pokemon.sh
@@ -2,7 +2,9 @@ curl "https://pokeapi.co/api/v2/pokemon?limit=2" | jq -c '.results[]' | while re
   url=$(echo "$pokemons" | jq -r '.url')
   curl "$url" | jq -c '{id: .id, name: .name, sprite: .sprites.front_default}' | while read -r pokemon; do
     name=$(echo "$pokemon" | jq -r '.name')
-    echo "$pokemon"
+    id=$(echo "$pokemon" | jq -r '.id')
+    sprite=$(echo "$pokemon" | jq -r '.sprite')
+    echo "$name, $id, $sprite"
   done
 done
 
